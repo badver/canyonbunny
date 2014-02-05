3d32a0f6d2f26cb6821c15120fd3d5baca4562f7
badver <badver@mail.ru>
2014-02-05 18:27:06 +0400

    add actors

---
+++ CanyonBunny/src/ru/badver/study/canyonbunny/game/objects/GoldCoin.java
@@ -0,0 +1,41 @@
+package ru.badver.study.canyonbunny.game.objects;
+
+import ru.badver.study.canyonbunny.game.Assets;
+
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
+
+public class GoldCoin extends AbstractGameObject {
+	private TextureRegion regGoldCoin;
+	public boolean collected;
+
+	public GoldCoin() {
+		init();
+	}
+
+	private void init() {
+		dimension.set(0.5f, 0.5f);
+		regGoldCoin = Assets.instance.goldCoin.goldCoin;
+		// Set bounding box for collision detection
+		bounds.set(0, 0, dimension.x, dimension.y);
+		collected = false;
+	}
+
+	@Override
+	public void render(SpriteBatch batch) {
+		if (collected)
+			return;
+		TextureRegion reg = null;
+		reg = regGoldCoin;
+		batch.draw(reg.getTexture(), position.x, position.y, origin.x,
+				origin.y, dimension.x, dimension.y, scale.x, scale.y, rotation,
+				reg.getRegionX(), reg.getRegionY(), reg.getRegionWidth(),
+				reg.getRegionHeight(), false, false);
+
+	}
+
+	public int getScore() {
+		return 100;
+	}
+
+}
