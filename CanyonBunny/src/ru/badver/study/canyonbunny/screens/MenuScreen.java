b3c0da3c90ddd0f2a87ae5b54255cf21db863a1f
badver <badver@mail.ru>
2014-02-07 15:37:25 +0400

    add monochrome filter shader

--- CanyonBunny/src/ru/badver/study/canyonbunny/screens/MenuScreen.java
+++ CanyonBunny/src/ru/badver/study/canyonbunny/screens/MenuScreen.java
@@ -55,6 +55,7 @@ public class MenuScreen extends AbstractGameScreen {
 	private SelectBox selCharSkin;
 	private Image imgCharSkin;
 	private CheckBox chkShowFpsCounter;
+	private CheckBox chkUseMonochromeShader;
 
 	// debug
 	private final float DEBUG_REBUILD_INTERVAL = 5.0f;
@@ -75,6 +76,7 @@ public class MenuScreen extends AbstractGameScreen {
 		selCharSkin.setSelection(prefs.charSkin);
 		onCharSkinSelected(prefs.charSkin);
 		chkShowFpsCounter.setChecked(prefs.showFpsCounter);
+		chkUseMonochromeShader.setChecked(prefs.useMonochromeShader);
 	}
 
 	private void saveSettings() {
@@ -85,6 +87,7 @@ public class MenuScreen extends AbstractGameScreen {
 		prefs.volMusic = sldMusic.getValue();
 		prefs.charSkin = selCharSkin.getSelectionIndex();
 		prefs.showFpsCounter = chkShowFpsCounter.isChecked();
+		prefs.useMonochromeShader = chkUseMonochromeShader.isChecked();
 		prefs.save();
 	}
 
@@ -363,6 +366,13 @@ public class MenuScreen extends AbstractGameScreen {
 		tbl.add(new Label("Show FPS Counter", skinLibgdx));
 		tbl.add(chkShowFpsCounter);
 		tbl.row();
+
+		// + Checkbox, "Use Monochrome Shader" label
+		chkUseMonochromeShader = new CheckBox("", skinLibgdx);
+		tbl.add(new Label("Use Monochrome Shader", skinLibgdx));
+		tbl.add(chkUseMonochromeShader);
+		tbl.row();
+
 		return tbl;
 	}
 
--- CanyonBunny/src/ru/badver/study/canyonbunny/util/GamePreferences.java
+++ CanyonBunny/src/ru/badver/study/canyonbunny/util/GamePreferences.java
@@ -16,6 +16,7 @@ public class GamePreferences {
 	public int charSkin;
 	public boolean showFpsCounter;
 	private Preferences prefs;
+	public boolean useMonochromeShader;
 
 	// singleton: prevent instantiation from other classes
 	private GamePreferences() {
@@ -31,6 +32,7 @@ public class GamePreferences {
 				.clamp(prefs.getFloat("volMusic", 0.5f), 0.0f, 1.0f);
 		charSkin = MathUtils.clamp(prefs.getInteger("charSkin", 0), 0, 2);
 		showFpsCounter = prefs.getBoolean("showFpsCounter", false);
+		useMonochromeShader = prefs.getBoolean("useMonochromeShader", false);
 	}
 
 	public void save() {
@@ -40,6 +42,7 @@ public class GamePreferences {
 		prefs.putFloat("volMusic", volMusic);
 		prefs.putInteger("charSkin", charSkin);
 		prefs.putBoolean("showFpsCounter", showFpsCounter);
+		prefs.putBoolean("useMonochromeShader", useMonochromeShader);
 		prefs.flush();
 	}
 
