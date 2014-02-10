d524213ded9c45f4197dea55ff0902c562455a40
badver <badver@mail.ru>
2014-02-10 16:18:49 +0400

    comments

--- CanyonBunny-desktop/src/ru/badver/study/canyonbunny/Main.java
+++ CanyonBunny-desktop/src/ru/badver/study/canyonbunny/Main.java
@@ -10,17 +10,21 @@ public class Main {
 	private static boolean drawDebugOutline = false;
 
 	public static void main(String[] args) {
+		// перестраиваем атлас, если нужно
 		if (rebuildAtlas) {
 			Settings settings = new Settings();
 			settings.maxWidth = 1024;
 			settings.maxHeight = 1024;
 			settings.debug = drawDebugOutline;
+			
+			// упаковываем картинки
 			TexturePacker.process(settings, "assets-raw/images",
 					"../CanyonBunny-android/assets/images", "canyonbunny.pack");
+			
+			// упаковываем элементы интерфейса
 			TexturePacker.process(settings, "assets-raw/images-ui",
 					"../CanyonBunny-android/assets/images",
 					"canyonbunny-ui.pack");
-
 		}
 
 		LwjglApplicationConfiguration cfg = new LwjglApplicationConfiguration();
@@ -28,6 +32,9 @@ public class Main {
 		cfg.useGL20 = true;
 		cfg.width = 800;
 		cfg.height = 480;
+		cfg.fullscreen=false;
+		cfg.resizable=false;
+		
 
 		new LwjglApplication(new CanyonBunnyMain(), cfg);
 	}
